431e21b3d88b5f4962a8a45f5c289c63ee9d8f46
psarangi550 <psarangi50@gmail.com>
2023-08-19 19:56:15 +0530

    DSA Practise

--- DSA_Practises/list_coding_questions/comprehension_in_list.py
+++ DSA_Practises/list_coding_questions/comprehension_in_list.py
@@ -20,7 +20,7 @@ print(list2)
 
 
 # conditional List Element in here
-# the structure of the List comprehension with conition will be of `[<new item> for <item> in <list> if <condition>]`
+# the structure of the List comprehension with conition will be of `[<new item> for <item> in <list> if <condition>]` or `[<new item> if <condition> else <value> for <item> in <list>]`
 # this will help in creating the new element if the condition is matched or met
 
 prev_list1:List[int]=[-1,10,-20,2,-90,60,45,20]
@@ -36,3 +36,30 @@ print(new_list)
 new_negetive_square_list:List[int]=[item**2 for item in prev_list1 if item < 0]
 
 print(new_negetive_square_list)
+
+
+# fetch the consonant over here 
+
+sentence:str="My Name is Pratik"
+
+# now fetching the value as below which will fetch the consonant only
+
+list_of_consonants:List[str]=[sent for sent in sentence if sent not in ["a","e","i","o","u"]+list(map(str.upper,["a","e","i","o","u"])) and sent.isalpha()]
+
+print(list_of_consonants)
+
+# when number less than 0 then consider that as 0 else the number itself
+
+list_mod:List[int]=[item if item > 0 else 0 for item in prev_list1 ]
+
+print(list_mod)
+
+# another approach 
+
+def getNumber(number:int)->int:
+    
+    return number if number > 0 else 0
+
+list_mod1:List[int]=[getNumber(item) for item in prev_list1]
+
+print(list_mod1) 
